43cb9dbb19726e9ed511f7685773a245e8c10eba
ayushkumar107 <ayushbaranwal1472@gmail.com>
2025-11-23 18:01:31 +0530

    added DELETE API in my Todo

--- controllers/app.js
+++ controllers/app.js
@@ -1,42 +1,60 @@
-const List=require("../model/note");
+const List = require("../model/note");
 
-async function handleCreateNewTodo(req,res) {
-    const {title,description}=req.body;
-if (!title) return res.status(400).json({message:"title is required"});
-    const newTodo=await List.create({
-        title,
-        description,
-        isCompleted:false
-    });
-    
-    console.log(newTodo);
-    return res.status(201).json({message:"todo created succesfully",data:newTodo});
-};
+async function handleCreateNewTodo(req, res) {
+  const { title, description } = req.body;
+  if (!title) return res.status(400).json({ message: "title is required" });
+  const newTodo = await List.create({
+    title,
+    description,
+    isCompleted: false,
+  });
 
+  console.log(newTodo);
+  return res
+    .status(201)
+    .json({ message: "todo created succesfully", data: newTodo });
+}
 
-async function handleViewAllTODO(req,res) {
-    const allTodo=await List.find({});
-    return res.send(allTodo);
-    
-};
+async function handleViewAllTODO(req, res) {
+  const allTodo = await List.find({});
+  return res.send(allTodo);
+}
 
-async function handleTodoToggleCompletion(req,res) {
-    try{
-    
-    const list= await List.findById(req.params.id);
-    if(!list) return res.status(404).json({message:"todo id not founded"});
-    list.isCompleted=!list.isCompleted;
+async function handleTodoToggleCompletion(req, res) {
+  try {
+    const list = await List.findById(req.params.id);
+    if (!list) return res.status(404).json({ message: "todo id not founded" });
+    list.isCompleted = !list.isCompleted;
 
     await list.save();
-    return res.status(200).json({message:"todo completion status updated"});
-    }catch(error){
-        return res.status(500).json({message:"error updating status",error:error.message,})
+    return res.status(200).json({ message: "todo completion status updated" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "error updating status", error: error.message });
+  }
+}
+
+async function handleDeleteTodo(req, res) {
+  try {
+    const deleteTODO = await List.findByIdAndDelete(req.params.id);
+    if (!deleteTODO) {
+      return res.status(404).json({ msg: "TODO not founded..." });
     }
+
+    return res
+      .status(200)
+      .json({ msg: "TODO deleted successfully", deleteTODO });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "error while deleting TODO", error: error.message });
+  }
 }
 
-
-module.exports={
-    handleCreateNewTodo,
-    handleViewAllTODO,
-    handleTodoToggleCompletion,
-}
+module.exports = {
+  handleCreateNewTodo,
+  handleViewAllTODO,
+  handleTodoToggleCompletion,
+  handleDeleteTodo,
+};
--- routes/app.js
+++ routes/app.js
@@ -1,16 +1,16 @@
-const express=require("express");
+const express = require("express");
 
+const router = express.Router();
+const {
+  handleCreateNewTodo,
+  handleViewAllTODO,
+  handleTodoToggleCompletion,
+  handleDeleteTodo,
+} = require("../controllers/app");
 
-const router=express.Router();
-const {handleCreateNewTodo,handleViewAllTODO,handleTodoToggleCompletion}=require("../controllers/app")
+router.post("/", handleCreateNewTodo);
+router.get("/", handleViewAllTODO);
+router.patch("/:id/completed", handleTodoToggleCompletion);
+router.delete("/:id",handleDeleteTodo);
 
-
-
-router.post("/",handleCreateNewTodo);
-router.get("/",handleViewAllTODO);
-router.patch("/:id/completed",handleTodoToggleCompletion);
-
-
-module.exports=router;
-
-
+module.exports = router;

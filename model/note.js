8a517702c17a08f53d8026421b2b189915744923
ayushkumar107 <ayushbaranwal1472@gmail.com>
2025-11-23 10:31:13 +0530

    updated todo model

--- model/note.js
+++ model/note.js
@@ -5,9 +5,28 @@ const mongoose=require("mongoose");
 const noteSchema =new mongoose.Schema({
     title:{
         type:String,
+        required:true
 
     },
     description:{
         type:String,
+        default:"",
+
+    },
+    isCompleted:{
+        type:Boolean,
+        default:false,
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
     }
+},{
+    timestamps:true
 })
+
+const list=mongoose.model("todo",noteSchema);
+
+
+
+module.exports=list;
